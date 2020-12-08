05f555312275e34745753609f1f6c1e5b5b8c34e
Krzysztof Nadlonek <knadlonek@gmail.com>
2020-12-08 23:37:32 +0100

    slight change

--- src/actions/fetchAction.js
+++ src/actions/fetchAction.js
@@ -1,20 +1,17 @@
 import {GET_DAY,GET_WEEK} from "./types";
-const API_KEY = "af0a54b6bab5ad4a22f1f2b44a2b8491";
-const cors = "https://cors-anywhere.herokuapp.com/";
-// const blabla = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1ebdd7a28f6471618d7730875c5844fa";
-// const city = "London";
+// const API_KEY = "af0a54b6bab5ad4a22f1f2b44a2b8491";
 
+
+console.log(process.env,"env")
 
 export const getDay = (city) => async (dispatch) => {
     try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}` ,
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}` ,
 
       );
   
       if (res.ok) {
         const dayWeather = await res.json();
-
-        // console.log(dayWeather,"action")
 
         dispatch({
           type: GET_DAY,
@@ -33,7 +30,7 @@ export const getDay = (city) => async (dispatch) => {
 
   export const getWeek = (city) => async (dispatch) => {
     try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}` ,
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}` ,
 
       );
   
